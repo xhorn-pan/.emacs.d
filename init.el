a5288872e77184f2dae23452c29529e0850dbf1f
Xinghua Pan <xhorn.pan@gmail.com>
2017-11-20 12:09:01 -0500

    Update init.el

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 
 (package-initialize)
 (add-to-list 'load-path "~/.emacs.d/custom")
@@ -14,16 +16,3 @@
 
 (require 'yasnippet)
 (yas-global-mode 1)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (rtags company yasnippet dracula-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
